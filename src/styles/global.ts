e904f25b8af11e06f3aa5f72abff8a7fc0c9be10
Dailton Bastos <dailtonbastos@gmail.com>
2022-06-24 21:57:44 -0300

    chore: remove unsed files

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,16 +1,5 @@
 import { createGlobalStyle } from 'styled-components';
 
-const AppColors = {
-  primary: '#00c89c',
-  darkPrimary: '#00b48c',
-  gray: '#161718',
-  ligthGray: '#fff',
-  secondary: '#0090c8',
-  darkSecondary: '#0081b4',
-};
-
-export { AppColors };
-
 export const GlobalStyles = createGlobalStyle`
   * {
     box-sizing: border-box;
